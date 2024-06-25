6aad1676c21b5278249d70e2d3f74249a920ea2c
Jonathan Grizos <JonnyGabc@gmail.com>
2024-06-25 16:23:49 -0400

    Change config to handle type errors at runtime

--- lib/config.ts
+++ lib/config.ts
@@ -1,22 +1,25 @@
 import * as dotenv from 'dotenv';
 import path = require('path');
 
-dotenv.config({ path: path.resolve(__dirname, './.env')});
+dotenv.config({ path: path.resolve(__dirname, '../.env')});
 
 export type Env = {
-    PROJECT_NAME: string,
-    ENVIROMENT: string,
-    S3_IMAGE_BUCKET_NAME: string
+    readonly PROJECT_NAME: string,
+    readonly ENVIROMENT: string,
+    readonly S3_IMAGE_BUCKET_NAME: string
 }
-
-if (!process.env.PROJECT_NAME|| !process.env.ENVIROMENT || !process.env.S3_IMAGE_BUCKET_NAME) {
-    throw new Error('Env Not Configured Correctly');
+console.log(process.env);
+const ensureString = (value:any): string => {
+    if(!value || value.length ===0) {
+        throw new Error('Env Not Configured Correctly');
+    }
+    return value;
 }
 
 export const getConfig = (): Env => {
     return {
-        PROJECT_NAME: process.env.PROJECT_NAME || 'test',
-        ENVIROMENT: process.env.ENVIROMENT || 'test',
-        S3_IMAGE_BUCKET_NAME: process.env.S3_IMAGE_BUCKET_NAME || ''
+        PROJECT_NAME: ensureString(process.env.PROJECT_NAME),
+        ENVIROMENT: ensureString(process.env.ENVIROMENT),
+        S3_IMAGE_BUCKET_NAME: ensureString(process.env.S3_IMAGE_BUCKET_NAME)
     }
 }
